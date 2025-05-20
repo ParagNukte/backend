0a10d518c6f3103d9ae9571be927606e55b1338e
ParagNukte <paragnuktek@gmail.com>
2025-05-20 21:37:11 +0530

    Added fs.rm method.

--- Backend-Tutorial2/script.js
+++ Backend-Tutorial2/script.js
@@ -1,10 +1,19 @@
 import fs from "fs";
 
-// fs.writeFile("hey.txt", "Hello This is the new test file", (err) => {
-//   if (err) console.error(err);
-//   else console.log("File Created Successfully");
-// });
+fs.writeFile("hey.txt", "Hello This is the new test file", (err) => {
+  if (err) console.error(err);
+  else console.log("File Created Successfully");
+});
 fs.appendFile("hey.txt", "I am in continuation with this file", (err) => {
   if (err) console.error(err);
   else console.log("File Appneded Successfully");
+});
+
+fs.rename("hey.txt", "hello.txt", (err) => {
+  if (err) console.error(err);
+  else console.log("File Renamed Successfully");
+});
+fs.rm("hello.txt", { recursive: true }, (err) => {
+  if (err) console.error(err);
+  else console.log("File Deleted Successfully");
 });
